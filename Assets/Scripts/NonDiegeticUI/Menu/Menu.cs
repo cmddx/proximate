bcb02cc748920f11c8d04ab4054fca243f6d141d
Elliot <elliotherriman@gmail.com>
2024-09-29 01:46:43 +1000

    Opening and closing menu should now clear glitch material
    i'm sure there are better ways to do this, but if there's any chance the glitch effect gets stuck on, this should allow the player to clear it by opening and closing the menu. this will cause brief strangeness during timelines where the glitch strength is being manipulated, but we can fix this once we have time, and in the meantime, maybe a cosmetic glitch is better than a potential game breaking one?

--- Assets/Scripts/NonDiegeticUI/Menu/Menu.cs
+++ Assets/Scripts/NonDiegeticUI/Menu/Menu.cs
@@ -88,7 +88,7 @@ public class Menu : MonoBehaviour
         GetComponent<UnityEngine.UI.Image>().enabled = false;
         visorDisplay.SetActive(true);
 
-        glitchMat.SetFloat("_Strength", glitchMatValue);
+        // glitchMat.SetFloat("_Strength", glitchMatValue);
 
         if (cursorWasLocked)
         {
